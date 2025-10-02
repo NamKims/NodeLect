347bb5c04fc435f734fbcc46512f4dc629afbe97
namwoo.kim <donge1206@naver.com>
2025-10-02 15:42:35 +0900

    5주차

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const dotenv = require('dotenv');
+const indexRouter = require('./routes/index');
+
+
+dotenv.config();                    //process.env에 저장됨
+const app = express();
+
+
+app.set('port', process.env.PORT || 3000); //먼저 env.port를 쓰고 안되면 3000쓰세요 느낌
+app.use((req, res, next)=>{         //next는 매개변수로 활동
+    console.log('모든 요청에 모두 실행됩니다.');
+    next();                         //다음단계로 넘어간다
+}) 
+
+app.use(morgan('dev'));
+
+app.use('/', indexRouter);
+
+/*
+app.get('/about', (req, res)=> {
+    //res.send('Hello, World');
+    res.sendFile(path.join(__dirname, './index.html')); // /과 /about에 따라 사이트가 다른걸로 들어가짐
+    //next();
+});
+*/
+
+app.use((err,req,res,next)=> {
+    console.error(err);
+    res.status(500).send(err.message);
+})
+
+app.listen(app.get('port'), ()=> {
+    console.log(app.get('port'), '번 포트에서 대기  중');
+});
---
+++ routes/index.js
@@ -0,0 +1,14 @@
+//서버를 만들려는 것이 아닌, 서버 안쪽에 있는 라우터를 만들려고 하는거임.
+const express = require('express');
+const router = express.Router(); //라우터를 만들어야함
+const path = require('path');
+
+router.get('/', (req, res)=> {
+    res.sendFile(path.join(__dirname, '../ex/restFront.html'));
+});
+
+router.get('/about', (req, res)=> {
+    res.sendFile(path.join(__dirname, '../index.html'));
+});
+
+module.exports = router;
